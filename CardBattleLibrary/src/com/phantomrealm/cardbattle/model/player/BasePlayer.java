cb40d20d10fe65e6ef9c643321a019595bff8d16
Matthew Pape <matthew.pape@mapquest.com>
2014-02-20 21:00:16 -0500

    Adding working minimax player.

--- CardBattleLibrary/src/com/phantomrealm/cardbattle/model/player/BasePlayer.java
+++ CardBattleLibrary/src/com/phantomrealm/cardbattle/model/player/BasePlayer.java
@@ -1,28 +1,30 @@
-/**
- * 
- */
 package com.phantomrealm.cardbattle.model.player;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import com.phantomrealm.cardbattle.model.board.Board;
 import com.phantomrealm.cardbattle.model.board.Position;
 import com.phantomrealm.cardbattle.model.deck.Deck;
 
 /**
- * @author mpape
- *
+ * Abstract class which provides the basic functionality shared by all player types.
+ * 
+ * @author matthewpape
  */
 public abstract class BasePlayer implements Player {
 
+	protected PlayerIdentity mIdentity;
+	protected Deck mDeck;
+	
+	public BasePlayer(PlayerIdentity identity, Deck deck) {
+		mIdentity = identity;
+		mDeck = deck;
+	}
+	
 	/* (non-Javadoc)
 	 * @see com.phantomrealm.cardbattle.model.player.Player#getIdentity()
 	 */
 	@Override
 	public PlayerIdentity getIdentity() {
-		// TODO Auto-generated method stub
-		return null;
+		return mIdentity;
 	}
 
 	/* (non-Javadoc)
@@ -30,8 +32,7 @@ public abstract class BasePlayer implements Player {
 	 */
 	@Override
 	public Deck getDeck() {
-		// TODO Auto-generated method stub
-		return null;
+		return mDeck;
 	}
 
 	/* (non-Javadoc)
@@ -39,27 +40,5 @@ public abstract class BasePlayer implements Player {
 	 */
 	@Override
 	public abstract Position getMove(Board board, Deck opponentDeck);
-	
-	/**
-	 * Get a list of all the possible positions where the next card could be placed
-	 * 
-	 * @param board
-	 * @return a list (possibly empty but never null)
-	 */
-	protected List<Position> getPossibleMoves(Board board) {
-		List<Position> moves = new ArrayList<Position>();
-		int begin = getIdentity() == PlayerIdentity.LEFT_PLAYER ? 0 : board.getWidth() - 1;
-		int end = getIdentity() == PlayerIdentity.LEFT_PLAYER ? board.getWidth() - 1 : 0;
-		int increment = getIdentity() == PlayerIdentity.LEFT_PLAYER ? 1 : -1;
-		for (int row = 0; row < board.getHeight(); ++row) {
-			for (int col = begin; getIdentity() == PlayerIdentity.LEFT_PLAYER ? col <= end : col >= end; col += increment) {
-				if (board.getBoardSlot(row, col).getOwner() == null) {
-					moves.add(new Position(row, col));
-					break;
-				}
-			}
-		}
-		return moves;
-	}
 
 }
